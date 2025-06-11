655c5dc2bb6f99ddfa927cb8ba93ca7a5b1343bb
CBecke <christian.becke@outlook.dk>
2025-06-11 17:58:18 +0200

    laptop results

--- orgA/src/main/java/com/github/dapmthesis/orga/OrgAApplication.java
+++ orgA/src/main/java/com/github/dapmthesis/orga/OrgAApplication.java
@@ -43,10 +43,10 @@ public class OrgAApplication {
                 //"throughput/033ms_sleep_pipeline.json",
                 //"throughput/05ms_sleep_pipeline.json",
                 //"throughput/075ms_sleep_pipeline.json",
-                "throughput/1ms_sleep_pipeline.json"
+                //"throughput/1ms_sleep_pipeline.json"
 
                 //"alignment_pipeline.json",
-
+                "throughput/2ms_sleep_pipeline.json"
                 //"throughput/5ms_sleep_pipeline.json"
         );
 
@@ -100,7 +100,7 @@ public class OrgAApplication {
 
                 pipelineBuilder.buildPipeline(pipelineName, validatedPipeline);
                 executionService.start(pipelineName);
-                System.out.println("Experiment started (" + pipelineName + "), run " + (i + 1) + " of " + runs + ". Running for " + experimentLengthSeconds + " seconds.");
+                System.out.println("\n\nExperiment started (" + pipelineName + "), run " + (i + 1) + " of " + runs + ". Running for " + experimentLengthSeconds + " seconds.\n\n");
 
                 try { Thread.sleep(1000L * experimentLengthSeconds); }
                 catch (InterruptedException e) { Thread.currentThread().interrupt(); }
